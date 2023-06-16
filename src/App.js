616d655aded8982e7d509920425db1451b081ef0
adriannapinzariu <121136061+adriannapinzariu@users.noreply.github.com>
2023-06-15 23:05:31 -0500

    Made New Landing Page for Flock

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+import { Box, Heading, Text, Button } from '@chakra-ui/react';
+import birdFlying from './bird-flying.gif';
+import './App.css'; 
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box
+      bgGradient="linear(to-r,  #FAB3E5, #8BB0E9)" 
+      w='100vw'
+      h='100vh' 
+      display='flex'
+      flexDirection='column' 
+      alignItems='center' 
+      justifyContent='center' 
+      color='white'
+    >
+      <Box
+        display='flex'
+        flexDirection='column' 
+        alignItems='center' 
+        justifyContent='center' 
+        textAlign='center'
+        p={5}
+      >
+        <Heading mb={4}>FLOCK</Heading>
+        <Text fontSize='xl' mb={4}>We're working hard to bring you something amazing. Stay tuned.</Text>
+        <Button colorScheme='whiteAlpha'>Coming Soon</Button>
+      </Box>
+    </Box>
   );
 }
+
 
 export default App;
